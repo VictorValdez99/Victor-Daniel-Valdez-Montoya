d9c80d6a38d9e69a7afa6995211e4722c7858b9f
VictorValdez99 <107083651+VictorValdez99@users.noreply.github.com>
2022-06-13 20:13:41 -0600

    Add files via upload

---
+++ Ejercicio retador 1.py
@@ -0,0 +1,35 @@
+Superficie_Sinaloa = 57365
+print("La superficie del estado de Sinaloa es es:",Superficie_Sinaloa)
+
+Localizacion_Sinaloa = "Noroeste"
+print("La localizacion del estado de Sinaloa es:",Localizacion_Sinaloa)
+
+Clima_Sinaloa = " calido, subhumedo, seco, semiseco"
+print("El clima de Sinaloa es:",Clima_Sinaloa)
+
+Temperatura_Media = str(25.45)
+print("La Temperatura media es:",Temperatura_Media)
+
+Precipitacion_Media = 7901
+print("La precipitacionn media  es:",Precipitacion_Media)
+
+Poblacion_hombres = 1494815
+print("La poblacion de hombres es:",Poblacion_hombres)
+
+Poblacion_mujeres = 1532128
+print("La poblacion de mujeres es:",Poblacion_mujeres)
+
+Porcentaje_Culiacan = 33.15
+print("El porcentaje en Culiacan es del:",Porcentaje_Culiacan)
+
+Porcentaje_Mazatlan = 16.57
+print("El porcentaje en Mazatlan es del:",Porcentaje_Mazatlan)
+Poblacion_total = Poblacion_hombres + Poblacion_mujeres
+print("La poblacion total es de:",Poblacion_total)
+
+Porcentaje_Culiacan_Mazatlan = Porcentaje_Culiacan + Porcentaje_Mazatlan
+print("El porcentaje de Culiacan y Mazatlan es de:",Porcentaje_Culiacan_Mazatlan)
+
+Temp_Climas = Temperatura_Media  + Clima_Sinaloa
+print ("La temperatura media y el tipo de clima de Sinaloa son: ",  Temp_Climas)
+
